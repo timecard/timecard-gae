03cfc39a4077d94e11bc8ba82a09b5ca8cd2048a
MiCHiLU <djmchl@gmail.com>
2013-12-23 16:51:26 +0700

    Merge gae-tap
    91b0e2eafbeddc9b3a5e4690a98040c5dbb5f8c7 (Dec 23, 2013 2:55:35 PM)
    
    Conflicts:
    	README.rst

--- gae/appengine_config.py
+++ gae/appengine_config.py
@@ -8,13 +8,18 @@ config_APPS = {
   "<:localhost|timecard-gae.appspot.com>": (("/users", "app_users"),),
   #r"<subdomain:(?!www\.)[^.]+>.local": (("/test", "app_sample"),),
 }
+
+# This is a session secret key used by webapp2 framework.
+# Get 'a random and long string' from here:
+# http://clsc.net/tools/random-string-generator.php
+# or execute this from a python shell: import os; os.urandom(64)
+config_SECRET_KEY = "a very long and secret session key goes here"
+
 #config_APPSTATS_INCLUDE_ERROR_STATUS = False
+#config_FEEDBACK_FORMKEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 #config_GA_ACCOUNT = ""
 #config_JOB_EMAIL_RECIPIENT = "email@example.com"
-#config_FEEDBACK_FORMKEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#config_WEBAPP2_CONFIG = {}
 
 def webapp_add_wsgi_middleware(app):
   return recording.appstats_wsgi_middleware(app)
-
-#GOOGLE_USERNAME = "email@example.com"
-#GOOGLE_PASSWORD = "password"
