a32eb3691cc43e1499f0115ee9df910acccf221f
MiCHiLU <djmchl@gmail.com>
2013-12-23 17:59:51 +0700

    ログアウトURLへのリンクを表示する #6

--- gae/app_users.py
+++ gae/app_users.py
@@ -10,8 +10,10 @@ class SignIn(utils.RequestHandler):
   i18n_domain = "timecard-users"
 
   @utils.head(bootstrap)
-  #@utils.cache(60)
+  @utils.session_read_only
   def get(self, subdomain=None):
+    name = self.session.get("name")
+    is_signin = name is not None
     self.render_response("sign_in.html", locals())
 
 routes = [
