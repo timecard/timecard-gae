569b684025eef425308b3d21157ee4773abc190f
MiCHiLU <djmchl@gmail.com>
2013-12-26 09:03:55 +0700

    add settings page

--- gae/app_main.py
+++ gae/app_main.py
@@ -17,6 +17,22 @@ class Index(BaseHandler):
     is_signin = name is not None
     self.render_response("index.html", locals())
 
+class Settings(BaseHandler):
+  @utils.head(bootstrap)
+  @utils.session_read_only
+  def get(self):
+    name = self.session.get("name")
+    is_signin = name is not None
+    self.render_response("settings.html", locals())
+
+  @utils.head(bootstrap)
+  @utils.session
+  def post(self):
+    name = self.session.get("name")
+    is_signin = name is not None
+    self.render_response("settings.html", locals())
+
 routes = [
-  webapp2.Route("/", Index),
+  webapp2.Route("/", Index, name="index"),
+  webapp2.Route("/settings", Settings, name="settings"),
 ]
