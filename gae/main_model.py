96f0f6142c11b33b75d022bd9a67b5dfac1d84de
MiCHiLU <djmchl@gmail.com>
2013-12-28 14:09:17 +0700

    indexを最適化した

--- gae/main_model.py
+++ gae/main_model.py
@@ -2,11 +2,15 @@
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
-  #key = user.user_id()
+  #key
+  #user_id = ndb.StringProperty(indexed=True, required=True)
+
   name = ndb.StringProperty(indexed=False)
   #not_do_today = list of Issue
 
 class Project(ndb.Model):
+  #key auto
+
   name = ndb.StringProperty(indexed=False, required=True)
   description = ndb.TextProperty(indexed=False)
   is_public = ndb.BooleanProperty(indexed=False, default=True)
@@ -18,33 +22,39 @@ class Project(ndb.Model):
   #ruffnote
 
 class ArchivedComment(ndb.Model):
-  body = ndb.TextProperty(indexed=False, required=True)
-  author = ndb.StringProperty(indexed=False, required=True)
   datetime = ndb.DateTimeProperty(indexed=False, required=True)
+  author = ndb.StringProperty(indexed=False, required=True)
+  body = ndb.TextProperty(indexed=False, required=True)
 
 class Issue(ndb.Model):
+  #key
+  #project = ndb.KeyProperty(indexed=True, kind=Project, required=True)
+  #will_start_at = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
+  #author = ndb.StringProperty(indexed=False)
+
   subject = ndb.StringProperty(indexed=False, required=True)
   description = ndb.TextProperty(indexed=False)
-  will_start_at = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
   closed_on = ndb.DateTimeProperty(indexed=True)
-  project = ndb.KeyProperty(indexed=True, kind=Project, required=True)
-  author = ndb.StringProperty(indexed=False)
   assignee = ndb.KeyProperty(indexed=True, kind=User)
   comment = ndb.LocalStructuredProperty(ArchivedComment, indexed=False, repeated=True, compressed=True)
   #closedしたらCommentを格納する
 
 class WorkLoad(ndb.Model):
   #ユーザーは同時に複数のWorkLoadを作れない
+  #key
+  #project = ndb.KeyProperty(indexed=True, kind=Project, required=True)
+
   start_at = ndb.DateTimeProperty(indexed=False, required=True)
   end_at = ndb.DateTimeProperty(indexed=False)
   issue = ndb.KeyProperty(indexed=True, kind=Issue, required=True)
   user = ndb.KeyProperty(indexed=True, kind=User, required=True)
   active = ndb.BooleanProperty(indexed=True, default=True)
-  project = ndb.KeyProperty(indexed=True, kind=Project, required=True)
 
 class Comment(ndb.Model):
+  #key
+  #issue = ndb.KeyProperty(indexed=True, kind=Issue, required=True)
+  #datetime = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
+  #author = ndb.StringProperty(indexed=False, required=True)
+
   body = ndb.TextProperty(indexed=False, required=True)
-  issue = ndb.KeyProperty(indexed=True, kind=Issue, required=True)
-  author = ndb.StringProperty(indexed=False, required=True)
-  datetime = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
   project = ndb.KeyProperty(indexed=True, kind=Project, required=True)
