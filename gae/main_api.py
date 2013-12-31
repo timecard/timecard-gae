4f7764379dc3bf297e9520bc0ce1ee41e325ab45
MiCHiLU <djmchl@gmail.com>
2013-12-31 17:29:47 +0700

    refactor

--- gae/main_api.py
+++ gae/main_api.py
@@ -1,12 +1,12 @@
 from google.appengine.ext import ndb
 from protorpc import (
   message_types,
-  messages,
   remote,
 )
 import endpoints
 
 import main_model as model
+import main_message as message
 
 @ndb.tasklet
 def user_store(user):
@@ -17,32 +17,16 @@ def user_store(user):
   entity.name = user.name
   yield entity.put_async()
 
-class ProjectSend(messages.Message):
-  name        = messages.StringField (1, required=True)
-  description = messages.StringField (2, required=True)
-  is_public   = messages.BooleanField(3, required=True)
-  closed      = messages.BooleanField(4, required=True)
-  archive     = messages.BooleanField(5, required=True)
-  admin       = messages.StringField (6, repeated=True)
-  member      = messages.StringField (7, repeated=True)
-
-class ProjectReceive(messages.Message):
-  name        = messages.StringField (1, required=True)
-  description = messages.StringField (2, required=True)
-
-class ProjectSendCollection(messages.Message):
-  items = messages.MessageField(ProjectSend, 1, repeated=True)
-
 #package = "timecard"
 
 @endpoints.api(name="timecard", version="v1")
 class ProjectAPI(remote.Service):
 
-  @endpoints.method(message_types.VoidMessage, ProjectSend,
+  @endpoints.method(message_types.VoidMessage, message.ProjectSend,
                     path="projects", http_method="GET",
                     name="timecard.projects")
   def greetings_list(self, _request):
-    return ProjectSendCollection()
+    return message.ProjectSendCollection()
 
 api_services = [
   ProjectAPI,
