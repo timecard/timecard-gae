985e2041931313d58915effb80dcf28227674013
MiCHiLU <djmchl@gmail.com>
2014-01-02 18:24:30 +0700

    update project on API

--- gae/main_api_v1.py
+++ gae/main_api_v1.py
@@ -19,13 +19,27 @@ def user_store(user):
 
 api = endpoints.api(name="timecard", version="v1")
 
-@api.api_class(resource_name="project")
+@api.api_class(resource_name="project", path="project")
 class Project(remote.Service):
 
   @endpoints.method(message_types.VoidMessage, message.ProjectSend)
   def list(self, _request):
     return message.ProjectSendCollection()
 
-api_services = [
-  Project,
-]
+  @endpoints.method(message_types.VoidMessage, message.ProjectSend, name="create")
+  def create(self, _request):
+    return message.ProjectSendCollection()
+
+  @endpoints.method(message_types.VoidMessage, message.ProjectSend)
+  def read(self, _request):
+    return message.ProjectSendCollection()
+
+  @endpoints.method(message_types.VoidMessage, message.ProjectSend)
+  def update(self, _request):
+    return message.ProjectSendCollection()
+
+  @endpoints.method(message_types.VoidMessage, message.ProjectSend)
+  def delete(self, _request):
+    return message.ProjectSendCollection()
+
+api_services = [api]
