64667206e32c8596506986c965131ab646926557
MiCHiLU <djmchl@gmail.com>
2014-01-10 13:30:55 +0700

    update to gaetap-v1.0-beta-15-g964093c+

--- gae/tap/static/js/i18n_null.coffee
+++
@@ -1,5 +0,0 @@
-window.gettext = (msgid) ->
-  return msgid
-
-window.ngettext = (singular, plural, count) ->
-  return if count is 1 then singular else plural
--- gae/tap/tests/conf.py
+++
@@ -1,5 +0,0 @@
-import os
-
-root_path = os.path.dirname(os.path.abspath(os.path.dirname(os.path.dirname( __file__ ) + "/../..")))
-assert root_path.endswith("/gae")
-assert os.path.exists(root_path)
