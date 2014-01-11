86df3619f99475c4d070b3a5e682b2db160ef14f
MiCHiLU <djmchl@gmail.com>
2014-01-11 12:13:43 +0700

    refactor

--- gae/main_message.py
+++ gae/main_message.py
@@ -1,26 +1,18 @@
 from protorpc import messages
 
-class UserSend(messages.Message):
-  user_id     = messages.StringField (1, required=True)
-  name        = messages.StringField (2, required=True)
-  language    = messages.StringField (3, required=True)
 
 class UserReceive(messages.Message):
   name        = messages.StringField (1, required=True)
   language    = messages.StringField (2, required=True)
 
+class UserSend(messages.Message):
+  user_id     = messages.StringField (1, required=True)
+  name        = messages.StringField (2, required=True)
+  language    = messages.StringField (3, required=True)
+
 class UserSendCollection(messages.Message):
   items = messages.MessageField(UserSend, 1, repeated=True)
 
-class ProjectSend(messages.Message):
-  key         = messages.IntegerField(1, required=True)
-  name        = messages.StringField (2, required=True)
-  description = messages.StringField (3, required=True)
-  is_public   = messages.BooleanField(4, required=True)
-  closed      = messages.BooleanField(5, required=True)
-  archive     = messages.BooleanField(6, required=True)
-  admin       = messages.StringField (7, repeated=True)
-  member      = messages.StringField (8, repeated=True)
 
 class ProjectReceiveNew(messages.Message):
   name        = messages.StringField (1, required=True)
@@ -32,6 +24,16 @@ class ProjectReceive(messages.Message):
   name        = messages.StringField (2, required=True)
   description = messages.StringField (3, default="")
   is_public   = messages.BooleanField(4, default=True)
+
+class ProjectSend(messages.Message):
+  key         = messages.IntegerField(1, required=True)
+  name        = messages.StringField (2, required=True)
+  description = messages.StringField (3, required=True)
+  is_public   = messages.BooleanField(4, required=True)
+  closed      = messages.BooleanField(5, required=True)
+  archive     = messages.BooleanField(6, required=True)
+  admin       = messages.StringField (7, repeated=True)
+  member      = messages.StringField (8, repeated=True)
 
 class ProjectSendCollection(messages.Message):
   items = messages.MessageField(ProjectSend, 1, repeated=True)
