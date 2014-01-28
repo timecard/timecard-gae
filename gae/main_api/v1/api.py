43f6664af4dbd723f4d84cbb164f231f3d7837f9
ENDOH takanao <djmchl@gmail.com>
2014-01-28 17:23:11 +0700

    add description

--- gae/main_api/v1/api.py
+++ gae/main_api/v1/api.py
@@ -1,6 +1,7 @@
 import endpoints
 
 api = endpoints.api(name="timecard", version="dev",
+                    description = "timecard API",
                     allowed_client_ids = [
                       endpoints.API_EXPLORER_CLIENT_ID,
                       "636938638718.apps.googleusercontent.com",
