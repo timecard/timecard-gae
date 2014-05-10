1b2dff96d3f943e91b3891c6f5ac4dfe8383d656
ENDOH takanao <djmchl@gmail.com>
2014-05-10 17:39:29 +0530

    update allowed client IDs

--- gae/main_api/v1/api.py
+++ gae/main_api/v1/api.py
@@ -1,11 +1,15 @@
 import endpoints
 
+_allowed_client_ids = [
+  endpoints.API_EXPLORER_CLIENT_ID,
+  "636938638718.apps.googleusercontent.com", # web application
+  "636938638718-f7fose4kkhe2imf1c38l25fsamarn8ei.apps.googleusercontent.com", # Chrome application
+  "636938638718-q1shs6aa5rnflil339rhset2rs9fcq52.apps.googleusercontent.com", # iOS application
+]
+
 api = endpoints.api(name="timecard", version="dev",
                     description = "timecard API",
-                    allowed_client_ids = [
-                      endpoints.API_EXPLORER_CLIENT_ID,
-                      "636938638718.apps.googleusercontent.com",
-                    ],
+                    allowed_client_ids = _allowed_client_ids,
                    )
 
 api_services = [api]
